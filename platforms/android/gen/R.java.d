eefb082ed85f2de0d13077134ff2de3915099826
Bálint Tahi <tahi.balint@hotmail.com>
2013-09-18 13:26:25 +0200

    hello world type app

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+C:\Users\Tahi\buxapilot\platforms\android\gen\com\phonegap\hello_world\R.java \
+ : C:\Users\Tahi\buxapilot\platforms\android\res\drawable\icon.png \
+C:\Users\Tahi\buxapilot\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\Tahi\buxapilot\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\Users\Tahi\buxapilot\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\Tahi\buxapilot\platforms\android\res\drawable-ldpi\ic_launcher.png \
+C:\Users\Tahi\buxapilot\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\Tahi\buxapilot\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\Users\Tahi\buxapilot\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\Tahi\buxapilot\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\Tahi\buxapilot\platforms\android\res\layout\main.xml \
+C:\Users\Tahi\buxapilot\platforms\android\res\values\strings.xml \
+C:\Users\Tahi\buxapilot\platforms\android\res\xml\config.xml \
+C:\Users\Tahi\buxapilot\platforms\android\bin\AndroidManifest.xml \
