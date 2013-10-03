cab71580bb1556ec2b0363ae460aa2982c067f80
Bálint Tahi <tahi.balint@hotmail.com>
2013-10-03 15:49:32 +0200

    network and dev info

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,70 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		
+		var networkinfo = function(){
+				alert(navigator.network.connection.type);
+			}
+			
+			function openbarcode(){
+				var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+				scanner.scan(
+					function (result) {
+					  alert("We got a barcode\n" +
+							"Result: " + result.text + "\n" +
+							"Format: " + result.format + "\n" +
+							"Cancelled: " + result.cancelled);
+					}, 
+					function (error) {
+						alert("Scanning failed: " + error);
+					}
+				);
+			};
+			
+			function getcontacts(){
+				var options = new ContactFindOptions();
+				options.filter=""; 
+				options.multiple = true;
+				var fields = ["*"];
+				
+				function onSuccess(contacts){
+					$("#list").html('');
+					$(contacts).each(function(){
+						$("#list").append("<li>" + this.displayName + "</li>");
+					});
+				
+					console.log('Found: ' + contacts.length);
+				}
+				
+				function onError(contactError) {
+					alert('onError!');
+				};
+				
+				navigator.contacts.find(fields, onSuccess, onError, options);
+			}
+			
+			function geolocation(){
+				var onSuccess = function(position) {
+					alert('Latitude: '          + position.coords.latitude          + '\n' +
+						  'Longitude: '         + position.coords.longitude         + '\n' +
+						  'Altitude: '          + position.coords.altitude          + '\n' +
+						  'Accuracy: '          + position.coords.accuracy          + '\n' +
+						  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+						  'Heading: '           + position.coords.heading           + '\n' +
+						  'Speed: '             + position.coords.speed             + '\n' +
+						  'Timestamp: '         + position.timestamp                + '\n');
+				};
+
+				// onError Callback receives a PositionError object
+				//
+				function onError(error) {
+					alert('code: '    + error.code    + '\n' +
+						  'message: ' + error.message + '\n');
+				}
+
+				navigator.geolocation.getCurrentPosition(onSuccess, onError);
+			}
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,5 +109,13 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+		
+		var element = document.getElementById('deviceProperties');
+
+        element.innerHTML = 'Device Name: '     + device.name     + '<br />' + 
+                            'Device PhoneGap: ' + device.phonegap + '<br />' + 
+                            'Device Platform: ' + device.platform + '<br />' + 
+                            'Device UUID: '     + device.uuid     + '<br />' + 
+                            'Device Version: '  + device.version  + '<br />';
     }
 };
